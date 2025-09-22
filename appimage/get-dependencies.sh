0af94901fd933dc9ab7ed6b2aebfc7e3d8b42c07
zumbo666 <149120981+zumbo666@users.noreply.github.com>
2025-09-23 01:39:24 +0200

    Add files via upload

---
+++ appimage/get-dependencies.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	djvulibre        \
+	ghostscript      \
+	jbigkit          \
+	libheif          \
+	libjpeg-turbo    \
+	libjxl           \
+	libraw           \
+	librsvg          \
+	libtiff          \
+	libultrahdr      \
+	libwebp          \
+	libwmf           \
+	libzip           \
+	openexr          \
+	openjpeg2        \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	qt6ct            \
+	qt6-wayland      \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl --prefer-nano qt6-base-mini gtk3-mini libxml2-mini opus-mini
